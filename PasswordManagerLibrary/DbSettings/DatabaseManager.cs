01385c2be1691fba002b833851f27171f6f41495
mlakred <115311821+lakred@users.noreply.github.com>
2023-05-05 20:29:22 +0200

    second commit

--- PasswordManagerLibrary/DbSettings/DatabaseManager.cs
+++ PasswordManagerLibrary/DbSettings/DatabaseManager.cs
@@ -23,23 +23,22 @@ namespace PasswordManagerLibrary
 
         public void InsertUser(string email, string password)
         {
-            // Define the SQL statement to insert the new user
             var sql = "INSERT INTO users (usermail, Password) VALUES (@Email, @Password)";
 
-            // Create a new SqlConnection object using the connection string
+            
             using (var connection = new SqlConnection(connectionString))
             {
-                // Open the connection to the database
+               
                 connection.Open();
 
-                // Create a new SqlCommand object using the SQL statement and the SqlConnection object
+                
                 using (var command = new SqlCommand(sql, connection))
                 {
-                    // Add parameters to the SqlCommand object to set the values of the email and password fields
+                    
                     command.Parameters.AddWithValue("@Email", email);
                     command.Parameters.AddWithValue("@Password", password);
 
-                    // Execute the SQL command to insert the new user into the Users table
+                    
                     command.ExecuteNonQuery();
                 }
             }
